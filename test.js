a26ea8dba930491e5a2044eddea344abacc443e5
Michael Frenkel <michael.frenkel@outlook.com>
2019-09-10 16:52:53 -0700

    add a few more Tree methods

--- test.js
+++ test.js
@@ -13,7 +13,7 @@ class Tree {
     return output;
   };
 
-  mapInPlace = (callback) => {
+  mapInPlace(callback) {
     this.value = callback(this.value);
 
     for (let i = 0; i < this.children.length; i++) {
@@ -21,5 +21,38 @@ class Tree {
     }
   };
 
-  
+  addChild(child) {
+    if (!child || !(child instanceof Tree)) {
+      child = new Tree(child);
+    }
+
+    if (!this.isDescendant(child)) {
+      this.children.push(child);
+    } else {
+      throw new Error('That child is already ')
+    }
+    return child;
+  }
+
+  isDescendant(child) {
+    if (this.children.indexOf(child) !== -1) {
+      return true;
+    } else {
+      for (let i = 0; i < this.children.length; i++) {
+        if (this.children[i].isDescendant(child)) {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+
+  removeChild(child) {
+    const index = this.children.indexOf(child);
+    if (index !== -1) {
+      this.children.splice(index, 1);
+    } else {
+      throw new Error('That node is not an immediate child of tree');
+    }
+  }
 }
