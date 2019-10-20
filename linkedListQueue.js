c139060aeb5753fad3d6efdbe0706c6e09f2ecd3
Michael Frenkel <michael.frenkel@outlook.com>
2019-10-20 16:38:38 -0700

    implement a Queue using a Linked List

---
+++ linkedListQueue.js
@@ -0,0 +1,38 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class Queue {
+  constructor(front = null, back = null) {
+    this.front = front;
+    this.back = back;
+  }
+
+
+  enqueue() {
+    const node = new Node(element, null);
+    if (!this.back) {
+      this.front = node;
+      this.back = node;
+    } else {
+      this.back.next = node;
+      this.back = this.back.next;
+    }
+  }
+
+  dequeue() {
+    if (this.front) {
+      this.front = this.front.next;
+      return this.front.data;
+    } else {
+      if (this.back) {
+        this.back = null;
+      }
+      return 'Cannot dequeue because queue is empty';
+    }
+  }
+
+}
