8eb8fd6c01c22fa274c37ce79e0b44a74cd0f6b3
Michael Frenkel <michael.frenkel@outlook.com>
2019-10-21 11:58:02 -0700

    implement a queue from stacks

--- linkedListQueue.js
+++ linkedListQueue.js
@@ -1,3 +1,42 @@
+// class Node {
+//   constructor(data, next = null) {
+//     this.data = data;
+//     this.next = next;
+//   }
+// }
+
+// class Queue {
+//   constructor(front = null, back = null) {
+//     this.front = front;
+//     this.back = back;
+//   }
+
+
+//   enqueue() {
+//     const node = new Node(element, null);
+//     if (!this.back) {
+//       this.front = node;
+//       this.back = node;
+//     } else {
+//       this.back.next = node;
+//       this.back = this.back.next;
+//     }
+//   }
+
+//   dequeue() {
+//     if (this.front) {
+//       this.front = this.front.next;
+//       return this.front.data;
+//     } else {
+//       if (this.back) {
+//         this.back = null;
+//       }
+//       return 'Cannot dequeue because queue is empty';
+//     }
+//   }
+
+// }
+
 class Node {
   constructor(data, next = null) {
     this.data = data;
@@ -11,22 +50,23 @@ class Queue {
     this.back = back;
   }
 
-
-  enqueue() {
+  enqueue(element) {
     const node = new Node(element, null);
+
     if (!this.back) {
-      this.front = node;
-      this.back = node;
+      this.front = n;
+      this.back = n;
     } else {
-      this.back.next = node;
+      this.back.next = n;
       this.back = this.back.next;
     }
   }
 
   dequeue() {
     if (this.front) {
+      const front = this.front;
       this.front = this.front.next;
-      return this.front.data;
+      return front.data;
     } else {
       if (this.back) {
         this.back = null;
@@ -34,5 +74,4 @@ class Queue {
       return 'Cannot dequeue because queue is empty';
     }
   }
-
 }
